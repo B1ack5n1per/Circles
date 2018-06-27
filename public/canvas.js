e12c6c8c7c40d26c17e05a98af0a3df449912f60
Taylor Wong <taylorwong159@gmail.com>
2018-06-26 18:28:21 -0600

    added user input

--- public/canvas.js
+++ public/canvas.js
@@ -7,6 +7,33 @@ var height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
+var mouse = {
+  x: undefined,
+  y: undefined,
+};
+
+var colors = [
+  '#252525',
+  '#55565d',
+  '#a7adb1',
+  '#e1e8eb',
+  '#ff3b3b',
+];
+
+function chooseColor(num) {
+  return colors[num];
+};
+
+function over(event) {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  console.log(mouse);
+};
+
+window.addEventListener('mousemove', (event) => {
+  over(event);
+});
+
 function Circle(x, y, dx, dy, rad, col) {
   this.x = x;
   this.y = y;
@@ -24,6 +51,14 @@ function Circle(x, y, dx, dy, rad, col) {
   };
 
   this.update = () => {
+    if (mouse.x - this.x < 50 && mouse.x - this.x > -50 & mouse.y - this.y < 50 && mouse.y - this.y > -50) {
+      if (this.radius < 25) {
+        this.radius += 1;
+      };
+    } else if (this.radius > 2) {
+      this.radius -= 1;
+    }
+
     if (this.x >= width - this.radius || this.x <= this.radius) {
       this.dx = -this.dx;
     };
@@ -38,14 +73,14 @@ function Circle(x, y, dx, dy, rad, col) {
 };
 
 var circles = [];
-var numCircles = Math.floor(Math.random() * 25 + 10);
+var numCircles = 750;
 for (let i = 0; i < numCircles; i++) {
-  let dx = (Math.random() - 0.5) * 8;
-  let dy = (Math.random() - 0.5) * 8;
+  let dx = (Math.random() - 0.5) * 5;
+  let dy = (Math.random() - 0.5) * 5;
   let x = Math.random() * width;
   let y = Math.random() * height;
-  let radius = Math.random() * 90 + 10;
-  let color = 'rgba(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.random() + ')';
+  let radius = 2;
+  let color = chooseColor(Math.floor(Math.random() * 5));
   if (x < radius) {
     x += radius;
   } else if (x > width - radius) {
